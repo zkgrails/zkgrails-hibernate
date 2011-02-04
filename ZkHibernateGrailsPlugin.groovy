f879682f733549142ae3ad5ebc1d2d346776d9d6
Chanwit Kaewkasi <chanwit@gmail.com>
2011-02-04 19:11:42 +0700

    init zk-hibernate

---
+++ ZkHibernateGrailsPlugin.groovy
@@ -0,0 +1,50 @@
+class ZkHibernateGrailsPlugin {
+    // the plugin version
+    def version = "1.0.2-CL"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "1.2 > *"
+    // the other plugins this plugin depends on
+    def dependsOn = [zk:version]
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+            "grails-app/views/error.gsp"
+    ]
+
+    // TODO Fill in these fields
+    def author = "Chanwit Kaewkasi"
+    def authorEmail = "chanwit@gmail.com"
+    def title = "Hibernate support for ZKGrails"
+    def description = '''\\
+This is the ZK/Hibernate sub-plugin for Grails.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://grails.org/plugin/zk-hibernate"
+
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional), this event occurs before 
+    }
+
+    def doWithSpring = {
+        // TODO Implement runtime spring config (optional)
+    }
+
+    def doWithDynamicMethods = { ctx ->
+        // TODO Implement registering dynamic methods to classes (optional)
+    }
+
+    def doWithApplicationContext = { applicationContext ->
+        // TODO Implement post initialization spring config (optional)
+    }
+
+    def onChange = { event ->
+        // TODO Implement code that is executed when any artefact that this plugin is
+        // watching is modified and reloaded. The event contains: event.source,
+        // event.application, event.manager, event.ctx, and event.plugin.
+    }
+
+    def onConfigChange = { event ->
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
+}
