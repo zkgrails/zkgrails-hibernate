c8cdfcc19e33b1e20d4eefbed79b2c8f5a87afb8
Chanwit Kaewkasi <chanwit@gmail.com>
2011-09-09 00:29:25 +0700

    bump version to 1.1.M2

--- ZkHibernateGrailsPlugin.groovy
+++ ZkHibernateGrailsPlugin.groovy
@@ -4,7 +4,7 @@ import grails.util.*
 
 class ZkHibernateGrailsPlugin {
     // the plugin version
-    def version = "1.1.BUILD-SNAPSHOT"
+    def version = "1.1.M2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2 > *"
     // the other plugins this plugin depends on
