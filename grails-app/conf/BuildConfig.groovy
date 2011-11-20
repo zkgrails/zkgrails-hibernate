31332ee0eb4903d58932d46896e2325221f7fdc8
Chanwit Kaewkasi <chanwit@gmail.com>
2011-11-20 20:15:04 +0700

    fix dependency problem

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -12,27 +12,26 @@ grails.project.dependency.resolution = {
     repositories {
         grailsPlugins()
         grailsHome()
+        mavenRepo "http://zkgrails.googlecode.com/svn/repo/"
         grailsCentral()
 
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
         //mavenLocal()
-        //mavenCentral()
-        mavenRepo "http://zkgrails.googlecode.com/svn/repo/"
+        //mavenCentral()        
         //mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
-        // runtime ("org.zkoss.zk:zhtml:5.0.9") { transitive = false }
+        compile "org.zkoss.zk.grails:zk:5.0.9"
         build ("com.google.code.maven-svn-wagon:maven-svn-wagon:1.4") {
             export = false
         }
     }
     plugins {
-        runtime (":hibernate:$grailsVersion")
-        runtime (":zk:2.0.0.BUILD-SNAPSHOT")
+        compile ":hibernate:$grailsVersion"
         build(":tomcat:$grailsVersion",
               ":release:1.0.0.RC3",
               ":svn:1.0.0.M1") {
