8777cf058fe6eabb756a917fa4c54be28f93877a
Chanwit Kaewkasi <chanwit@gmail.com>
2012-02-14 13:31:26 +0700

    change verstion to M4 and make it works only for Grails 2

--- ZkHibernateGrailsPlugin.groovy
+++ ZkHibernateGrailsPlugin.groovy
@@ -4,9 +4,9 @@ import grails.util.*
 
 class ZkHibernateGrailsPlugin {
     // the plugin version
-    def version = "2.0.0.BUILD-SNAPSHOT"
+    def version = "2.0.0.M4"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3 > *"
+    def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
     // def dependsOn = [hibernate:"2.0.0.M2", zk:"${version}"]
     // resources that are excluded from plugin packaging
